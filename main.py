55ee7fac3619e980734034beaa62e28666d0f151
amine affif <affif.amine@live.fr>
2021-03-01 18:30:13 +0100

    we can fill a shape that is a list of a precise number of point objects

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# initialilasing the Point class
 class Point:
+
+    # basic point constructor
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -6,9 +9,28 @@ class Point:
         print("My x is : {}".format(self.x))
         print("My y is : {}".format(self.y))
 
+# Creating the list that handles all points
+shape = []
+
 # Get number of points
+numOfPoints = int(input("Enter the number of fence post: "))
+while numOfPoints < 0:
+    numOfPoints = numOfPoints = int(input("Enter the number of fence post: "))
 
 # Get position of each point
+
+shape = list()
+for i in range(0, numOfPoints):
+    shape.append(Point(1,5))
+    print("je rentre un point")
+
+print("--------------------------------------------------")
+print("--------------------------------------------------")
+print("--------------------------------------------------")
+
+for point in shape:
+    print(point.x)
+    
 
 # Display the area of the shape
 
