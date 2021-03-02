b0c407ac5e8aef167e879c2174cf7fa6b50aa95a
amine affif <affif.amine@live.fr>
2021-03-02 20:14:20 +0100

    factorized the added code in functions. Putting the user message in french. cleaning the code

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-#importing libraries
-
-
 # initialilasing the Point class
 class Point:
-
     # basic point constructor
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        print("I'm a constructed point")
-        print("My x is : {}".format(self.x))
-        print("My y is : {}".format(self.y))
-
+        print("Je suis un picket entre vos mains")
+        print("Ma position X : {}".format(self.x))
+        print("Ma position Y : {}".format(self.y))
 
 
 # Get number of points
@@ -21,54 +16,65 @@ def getNumOfPoints():
     shape = list()
     # input the number of points
     global numOfPoints
-    numOfPoints = int(input("Enter the number of fence post: "))
+    numOfPoints = int(input("Entrez le nombre de pickets : "))
     while numOfPoints < 0:
-        numOfPoints = numOfPoints = int(input("Enter the number of fence post: "))
+        numOfPoints = numOfPoints = int(input("Entrez le nombre de pickets : "))
     print("\n")
 
-getNumOfPoints()
 
 # Get X and Y position of each points
 def getPointsPosition():
     for i in range(0, numOfPoints):
-        print("Enter X position of the fence post {}".format(i+1))
+        print("Entrez la position X du picket {}".format(i+1))
         x = float(input(""))
-        print("Enter Y position of the fence post {}".format(i+1))
+        print("Entrez la position Y du picket {}".format(i+1))
         y = float(input(""))
 
         # insert point into the shape list
         shape.append(Point(x,y))
-        print("I inserted a point")
+        print("Picket planté !")
         print("\n")
 
-getPointsPosition()
-
-
-    
 
 # Display the area of the shape
-print("------------------------------------------")
+def getArea():
+    print("------------------------------------------")
 
-area = 0
-for i in range(len(shape)-1):
-        area += (shape[i].x * shape[i+1].y - shape[i+1].x * shape[i].y)/2
-print("The area of the fence is : {}".format(abs(area)))
+    global area
+    area = 0
+    for i in range(len(shape)-1):
+            area += (shape[i].x * shape[i+1].y - shape[i+1].x * shape[i].y)/2
+    print("L'aire est égale à : {}".format(abs(area)))
 
-print("------------------------------------------")
+    print("------------------------------------------")
 
 
 # Display the center of gravity
-x_center = 0
-y_center = 0
+def getCenterGravity():
+    print("------------------------------------------")
+    global x_center
+    global y_center
 
-for i in range(len(shape)-1):
-        x_center += ((shape[i].x + shape[i+1].x) * (shape[i].x * shape[i+1].y - shape[i+1].x * shape[i].y)) * (1/6 * abs(area))
-for i in range(len(shape)-1):
-        y_center += ((shape[i].y + shape[i+1].y) * (shape[i].x * shape[i+1].y - shape[i+1].x * shape[i].y)) * (1/6 * abs(area))
+    x_center = 0
+    y_center = 0
 
-print("Position du centre de gravité :")
-print("X : " + format((abs(x_center))) )
-print("Y : " + format((abs(y_center))) )
+    for i in range(len(shape)-1):
+            x_center += ((shape[i].x + shape[i+1].x) * (shape[i].x * shape[i+1].y - shape[i+1].x * shape[i].y)) * (1/6 * abs(area))
+    for i in range(len(shape)-1):
+            y_center += ((shape[i].y + shape[i+1].y) * (shape[i].x * shape[i+1].y - shape[i+1].x * shape[i].y)) * (1/6 * abs(area))
+
+    print("Position du centre de gravité :")
+    print("X : " + format((abs(x_center))) )
+    print("Y : " + format((abs(y_center))) )
+
+    print("------------------------------------------")
 
 
 # Display if the cow is outside the fence or not
+
+
+# Call all the functions
+getNumOfPoints()
+getPointsPosition()
+getArea()
+getCenterGravity()
